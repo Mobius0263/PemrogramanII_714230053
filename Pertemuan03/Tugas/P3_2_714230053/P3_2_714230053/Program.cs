7b7a7504dd0d1313a112997b59b1446587bf22d3
Mobis0263 <mahendra33115@gmail.com>
2024-10-28 23:51:46 +0700

    Tugas Pertemuan ke 3

---
+++ Pertemuan03/Tugas/P3_2_714230053/P3_2_714230053/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace P3_2_714230053
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool repeat = false;
+
+            do
+            {
+                Console.Clear();
+                Console.Write("=== Menu Persegi Panjang ===");
+                Console.Write("\n1. Hitung Luas");
+                Console.Write("\n2. Hitung Keliling");
+                Console.Write("\n3. Keluar");
+                Console.Write("\nPilih Menu (1-3): ");
+
+                int pilihan;
+                if (!int.TryParse(Console.ReadLine(), out pilihan) || pilihan < 1 || pilihan > 3)
+                {
+                    Console.WriteLine("Pilihan invalid. Tolong pilih angka 1, 2, or 3.");
+                    continue;
+                }
+
+                if (pilihan == 3)
+                {
+                    Console.WriteLine("Keluar...");
+                    break;
+                }
+
+                Console.Write("Masukkan panjang: ");
+                double panjang = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Masukkan lebar: ");
+                double lebar = Convert.ToDouble(Console.ReadLine());
+
+                if (pilihan == 1)
+                {
+                    double luas = panjang * lebar;
+                    Console.WriteLine("Luas persegi panjang: " + luas);
+                }
+                else if (pilihan == 2)
+                {
+                    double keliling = 2 * (panjang + lebar);
+                    Console.WriteLine("Keliling persegi panjang: " + keliling);
+                }
+
+                Console.WriteLine("\nIngin mengulang kembali? (Y/N)");
+                string restartpilihan = Console.ReadLine().ToUpper();
+                repeat = restartpilihan == "Y";
+
+                if (restartpilihan == "N")
+                {
+                    Console.WriteLine("Terima Kasih!");
+                }
+
+            } while (repeat);
+        }
+    }
+}
