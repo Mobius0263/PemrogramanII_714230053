c20a0674f66329d2bd17dc42f2c3cf70d1095a1d
Mobis0263 <mahendra33115@gmail.com>
2024-11-04 21:22:47 +0700

    Praktikum pertemuan ke 4

---
+++ Pertemuan04/Praktikum/P4_1_714230053/P4_1_714230053/ProductTest_714230053.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P4_1_714230053
+{
+    class ProductTest_714230053
+    {
+        static void Main(string[] args)
+        {
+            Book_714230053 product1 = new Book_714230053("Book", "C# Object Oriented Solution", "300");
+            DVD_714230053 product2 = new DVD_714230053("Ethernal Sunshine Of The Spotless Mind", "145");
+
+            product1.DisplayInfo();
+            product2.DisplayInfo();
+        }
+    }
+}
