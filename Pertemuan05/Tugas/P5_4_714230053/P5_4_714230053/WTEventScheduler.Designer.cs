117629943f0dfcaf0c21d70fe63bb64bac1c41b1
Mobis0263 <mahendra33115@gmail.com>
2024-11-12 00:07:08 +0700

    Tugas pertemuan ke 5

---
+++ Pertemuan05/Tugas/P5_4_714230053/P5_4_714230053/WTEventScheduler.Designer.cs
@@ -0,0 +1,377 @@
+﻿namespace P5_4_714230053
+{
+    partial class WTEventScheduler
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.name = new System.Windows.Forms.Label();
+            this.gender = new System.Windows.Forms.Label();
+            this.birthdate = new System.Windows.Forms.Label();
+            this.unit = new System.Windows.Forms.GroupBox();
+            this.option4 = new System.Windows.Forms.CheckBox();
+            this.option3 = new System.Windows.Forms.CheckBox();
+            this.option2 = new System.Windows.Forms.CheckBox();
+            this.option1 = new System.Windows.Forms.CheckBox();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.genderBox = new System.Windows.Forms.ComboBox();
+            this.birthdateBox = new System.Windows.Forms.DateTimePicker();
+            this.gamemode = new System.Windows.Forms.GroupBox();
+            this.gmoption4 = new System.Windows.Forms.RadioButton();
+            this.gmoption3 = new System.Windows.Forms.RadioButton();
+            this.gmoption2 = new System.Windows.Forms.RadioButton();
+            this.gmoption1 = new System.Windows.Forms.RadioButton();
+            this.time = new System.Windows.Forms.GroupBox();
+            this.tmoption5 = new System.Windows.Forms.RadioButton();
+            this.tmoption4 = new System.Windows.Forms.RadioButton();
+            this.tmoption3 = new System.Windows.Forms.RadioButton();
+            this.tmoption2 = new System.Windows.Forms.RadioButton();
+            this.tmoption1 = new System.Windows.Forms.RadioButton();
+            this.done = new System.Windows.Forms.Button();
+            this.make = new System.Windows.Forms.Button();
+            this.unit.SuspendLayout();
+            this.gamemode.SuspendLayout();
+            this.time.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // name
+            // 
+            this.name.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.name.Location = new System.Drawing.Point(64, 33);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(120, 25);
+            this.name.TabIndex = 0;
+            this.name.Text = "Name";
+            // 
+            // gender
+            // 
+            this.gender.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gender.Location = new System.Drawing.Point(64, 78);
+            this.gender.Name = "gender";
+            this.gender.Size = new System.Drawing.Size(120, 25);
+            this.gender.TabIndex = 1;
+            this.gender.Text = "Gender";
+            // 
+            // birthdate
+            // 
+            this.birthdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.birthdate.Location = new System.Drawing.Point(64, 123);
+            this.birthdate.Name = "birthdate";
+            this.birthdate.Size = new System.Drawing.Size(120, 25);
+            this.birthdate.TabIndex = 2;
+            this.birthdate.Text = "Birthdate";
+            // 
+            // unit
+            // 
+            this.unit.BackColor = System.Drawing.Color.Transparent;
+            this.unit.Controls.Add(this.option4);
+            this.unit.Controls.Add(this.option3);
+            this.unit.Controls.Add(this.option2);
+            this.unit.Controls.Add(this.option1);
+            this.unit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.unit.Location = new System.Drawing.Point(69, 213);
+            this.unit.Name = "unit";
+            this.unit.Size = new System.Drawing.Size(338, 401);
+            this.unit.TabIndex = 3;
+            this.unit.TabStop = false;
+            this.unit.Text = "Unit";
+            // 
+            // option4
+            // 
+            this.option4.BackColor = System.Drawing.Color.Gainsboro;
+            this.option4.Location = new System.Drawing.Point(34, 218);
+            this.option4.Name = "option4";
+            this.option4.Size = new System.Drawing.Size(147, 36);
+            this.option4.TabIndex = 3;
+            this.option4.Text = "Coastal Fleet";
+            this.option4.UseVisualStyleBackColor = false;
+            // 
+            // option3
+            // 
+            this.option3.BackColor = System.Drawing.Color.Gainsboro;
+            this.option3.Location = new System.Drawing.Point(34, 162);
+            this.option3.Name = "option3";
+            this.option3.Size = new System.Drawing.Size(147, 36);
+            this.option3.TabIndex = 2;
+            this.option3.Text = "Bluewater Fleet";
+            this.option3.UseVisualStyleBackColor = false;
+            // 
+            // option2
+            // 
+            this.option2.BackColor = System.Drawing.Color.Gainsboro;
+            this.option2.Location = new System.Drawing.Point(34, 106);
+            this.option2.Name = "option2";
+            this.option2.Size = new System.Drawing.Size(147, 36);
+            this.option2.TabIndex = 1;
+            this.option2.Text = "Ground";
+            this.option2.UseVisualStyleBackColor = false;
+            // 
+            // option1
+            // 
+            this.option1.BackColor = System.Drawing.Color.Gainsboro;
+            this.option1.Location = new System.Drawing.Point(34, 50);
+            this.option1.Name = "option1";
+            this.option1.Size = new System.Drawing.Size(147, 36);
+            this.option1.TabIndex = 0;
+            this.option1.Text = "Air";
+            this.option1.UseVisualStyleBackColor = false;
+            // 
+            // nameBox
+            // 
+            this.nameBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameBox.Location = new System.Drawing.Point(285, 33);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(377, 30);
+            this.nameBox.TabIndex = 4;
+            // 
+            // genderBox
+            // 
+            this.genderBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.genderBox.FormattingEnabled = true;
+            this.genderBox.Location = new System.Drawing.Point(285, 78);
+            this.genderBox.Name = "genderBox";
+            this.genderBox.Size = new System.Drawing.Size(377, 33);
+            this.genderBox.TabIndex = 5;
+            // 
+            // birthdateBox
+            // 
+            this.birthdateBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.birthdateBox.Location = new System.Drawing.Point(285, 123);
+            this.birthdateBox.Name = "birthdateBox";
+            this.birthdateBox.Size = new System.Drawing.Size(377, 30);
+            this.birthdateBox.TabIndex = 6;
+            // 
+            // gamemode
+            // 
+            this.gamemode.BackColor = System.Drawing.Color.Transparent;
+            this.gamemode.Controls.Add(this.gmoption4);
+            this.gamemode.Controls.Add(this.gmoption3);
+            this.gamemode.Controls.Add(this.gmoption2);
+            this.gamemode.Controls.Add(this.gmoption1);
+            this.gamemode.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gamemode.Location = new System.Drawing.Point(514, 213);
+            this.gamemode.Name = "gamemode";
+            this.gamemode.Size = new System.Drawing.Size(321, 401);
+            this.gamemode.TabIndex = 7;
+            this.gamemode.TabStop = false;
+            this.gamemode.Text = "Gamemode";
+            // 
+            // gmoption4
+            // 
+            this.gmoption4.BackColor = System.Drawing.Color.Gainsboro;
+            this.gmoption4.Location = new System.Drawing.Point(57, 218);
+            this.gmoption4.Name = "gmoption4";
+            this.gmoption4.Size = new System.Drawing.Size(147, 36);
+            this.gmoption4.TabIndex = 7;
+            this.gmoption4.TabStop = true;
+            this.gmoption4.Text = "Custom Battle";
+            this.gmoption4.UseVisualStyleBackColor = false;
+            // 
+            // gmoption3
+            // 
+            this.gmoption3.BackColor = System.Drawing.Color.Gainsboro;
+            this.gmoption3.Location = new System.Drawing.Point(57, 162);
+            this.gmoption3.Name = "gmoption3";
+            this.gmoption3.Size = new System.Drawing.Size(147, 36);
+            this.gmoption3.TabIndex = 6;
+            this.gmoption3.TabStop = true;
+            this.gmoption3.Text = "Simulator Battle";
+            this.gmoption3.UseVisualStyleBackColor = false;
+            // 
+            // gmoption2
+            // 
+            this.gmoption2.BackColor = System.Drawing.Color.Gainsboro;
+            this.gmoption2.Location = new System.Drawing.Point(57, 106);
+            this.gmoption2.Name = "gmoption2";
+            this.gmoption2.Size = new System.Drawing.Size(147, 36);
+            this.gmoption2.TabIndex = 5;
+            this.gmoption2.TabStop = true;
+            this.gmoption2.Text = "Realistic Battle";
+            this.gmoption2.UseVisualStyleBackColor = false;
+            // 
+            // gmoption1
+            // 
+            this.gmoption1.BackColor = System.Drawing.Color.Gainsboro;
+            this.gmoption1.Location = new System.Drawing.Point(57, 49);
+            this.gmoption1.Name = "gmoption1";
+            this.gmoption1.Size = new System.Drawing.Size(147, 36);
+            this.gmoption1.TabIndex = 4;
+            this.gmoption1.TabStop = true;
+            this.gmoption1.Text = "Arcade Battle";
+            this.gmoption1.UseVisualStyleBackColor = false;
+            // 
+            // time
+            // 
+            this.time.BackColor = System.Drawing.Color.Transparent;
+            this.time.Controls.Add(this.tmoption5);
+            this.time.Controls.Add(this.tmoption4);
+            this.time.Controls.Add(this.tmoption3);
+            this.time.Controls.Add(this.tmoption2);
+            this.time.Controls.Add(this.tmoption1);
+            this.time.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.time.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.time.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.time.Location = new System.Drawing.Point(942, 213);
+            this.time.Name = "time";
+            this.time.Size = new System.Drawing.Size(488, 401);
+            this.time.TabIndex = 8;
+            this.time.TabStop = false;
+            this.time.Text = "Time";
+            // 
+            // tmoption5
+            // 
+            this.tmoption5.BackColor = System.Drawing.Color.Gainsboro;
+            this.tmoption5.Location = new System.Drawing.Point(57, 272);
+            this.tmoption5.Name = "tmoption5";
+            this.tmoption5.Size = new System.Drawing.Size(372, 36);
+            this.tmoption5.TabIndex = 8;
+            this.tmoption5.TabStop = true;
+            this.tmoption5.Text = "Sunday, 1800 hours - 2200 hours";
+            this.tmoption5.UseVisualStyleBackColor = false;
+            // 
+            // tmoption4
+            // 
+            this.tmoption4.BackColor = System.Drawing.Color.Gainsboro;
+            this.tmoption4.Location = new System.Drawing.Point(57, 218);
+            this.tmoption4.Name = "tmoption4";
+            this.tmoption4.Size = new System.Drawing.Size(372, 36);
+            this.tmoption4.TabIndex = 7;
+            this.tmoption4.TabStop = true;
+            this.tmoption4.Text = "Sunday, 0900 hours - 1500 hours";
+            this.tmoption4.UseVisualStyleBackColor = false;
+            // 
+            // tmoption3
+            // 
+            this.tmoption3.BackColor = System.Drawing.Color.Gainsboro;
+            this.tmoption3.Location = new System.Drawing.Point(57, 162);
+            this.tmoption3.Name = "tmoption3";
+            this.tmoption3.Size = new System.Drawing.Size(372, 36);
+            this.tmoption3.TabIndex = 6;
+            this.tmoption3.TabStop = true;
+            this.tmoption3.Text = "Saturday, 1800 hours - 0000 hours";
+            this.tmoption3.UseVisualStyleBackColor = false;
+            // 
+            // tmoption2
+            // 
+            this.tmoption2.BackColor = System.Drawing.Color.Gainsboro;
+            this.tmoption2.Location = new System.Drawing.Point(57, 106);
+            this.tmoption2.Name = "tmoption2";
+            this.tmoption2.Size = new System.Drawing.Size(372, 36);
+            this.tmoption2.TabIndex = 5;
+            this.tmoption2.TabStop = true;
+            this.tmoption2.Text = "Saturday, 0900 hours - 1500 hours";
+            this.tmoption2.UseVisualStyleBackColor = false;
+            // 
+            // tmoption1
+            // 
+            this.tmoption1.BackColor = System.Drawing.Color.Gainsboro;
+            this.tmoption1.Location = new System.Drawing.Point(57, 49);
+            this.tmoption1.Name = "tmoption1";
+            this.tmoption1.Size = new System.Drawing.Size(372, 36);
+            this.tmoption1.TabIndex = 4;
+            this.tmoption1.TabStop = true;
+            this.tmoption1.Text = "Friday, 1800 hours - 0000 hours";
+            this.tmoption1.UseVisualStyleBackColor = false;
+            // 
+            // done
+            // 
+            this.done.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.done.Location = new System.Drawing.Point(912, 684);
+            this.done.Name = "done";
+            this.done.Size = new System.Drawing.Size(202, 58);
+            this.done.TabIndex = 9;
+            this.done.Text = "Done";
+            this.done.UseVisualStyleBackColor = true;
+            this.done.Click += new System.EventHandler(this.done_Click);
+            // 
+            // make
+            // 
+            this.make.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.make.Location = new System.Drawing.Point(462, 684);
+            this.make.Name = "make";
+            this.make.Size = new System.Drawing.Size(202, 58);
+            this.make.TabIndex = 10;
+            this.make.Text = "Make Card";
+            this.make.UseVisualStyleBackColor = true;
+            this.make.Click += new System.EventHandler(this.make_Click);
+            // 
+            // WTEventScheduler
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::P5_4_714230053.Properties.Resources.War_Thunder_Super_Resolution_2024_05_07___23_24_31_85_Thumbnail;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1577, 817);
+            this.Controls.Add(this.make);
+            this.Controls.Add(this.done);
+            this.Controls.Add(this.time);
+            this.Controls.Add(this.gamemode);
+            this.Controls.Add(this.birthdateBox);
+            this.Controls.Add(this.genderBox);
+            this.Controls.Add(this.nameBox);
+            this.Controls.Add(this.unit);
+            this.Controls.Add(this.birthdate);
+            this.Controls.Add(this.gender);
+            this.Controls.Add(this.name);
+            this.Name = "WTEventScheduler";
+            this.Text = "War Thunder Event Scheduler";
+            this.unit.ResumeLayout(false);
+            this.gamemode.ResumeLayout(false);
+            this.time.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label name;
+        private System.Windows.Forms.Label gender;
+        private System.Windows.Forms.Label birthdate;
+        private System.Windows.Forms.GroupBox unit;
+        private System.Windows.Forms.CheckBox option1;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.ComboBox genderBox;
+        private System.Windows.Forms.DateTimePicker birthdateBox;
+        private System.Windows.Forms.CheckBox option4;
+        private System.Windows.Forms.CheckBox option3;
+        private System.Windows.Forms.CheckBox option2;
+        private System.Windows.Forms.GroupBox gamemode;
+        private System.Windows.Forms.RadioButton gmoption4;
+        private System.Windows.Forms.RadioButton gmoption3;
+        private System.Windows.Forms.RadioButton gmoption2;
+        private System.Windows.Forms.RadioButton gmoption1;
+        private System.Windows.Forms.GroupBox time;
+        private System.Windows.Forms.RadioButton tmoption4;
+        private System.Windows.Forms.RadioButton tmoption3;
+        private System.Windows.Forms.RadioButton tmoption2;
+        private System.Windows.Forms.RadioButton tmoption1;
+        private System.Windows.Forms.RadioButton tmoption5;
+        private System.Windows.Forms.Button done;
+        private System.Windows.Forms.Button make;
+    }
+}
+
---
+++ Pertemuan05/Tugas/P5_4_714230053/P5_4_714230053/WTEventScheduler.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P5_4_714230053
+{
+    public partial class WTEventScheduler : Form
+    {
+        public WTEventScheduler()
+        {
+            InitializeComponent();
+
+            genderBox.Items.Add("<Select Gender>");
+            genderBox.Items.Add("Male");
+            genderBox.Items.Add("Female");
+
+            genderBox.SelectedIndex = 0;
+        }
+
+        private void make_Click(object sender, EventArgs e)
+        {
+            List<string> unit = new List<string>();
+            string gamemode = "";
+            string time = "";
+
+            if (option1.Checked ==  true)
+            {
+                unit.Add(option1.Text);
+            }
+            if (option2.Checked == true)
+            {
+                unit.Add(option2.Text);
+            }
+            if (option3.Checked == true)
+            {
+                unit.Add(option3.Text);
+            }
+            if (option4.Checked == true)
+            {
+                unit.Add(option4.Text);
+            }
+
+            if (gmoption1.Checked == true)
+            {
+                gamemode = gmoption1.Text;
+            }
+            else if (gmoption2.Checked == true)
+            {
+                gamemode = gmoption2.Text;
+            }
+            else if (gmoption3.Checked == true)
+            {
+                gamemode = gmoption3.Text;
+            }
+            else if (gmoption4.Checked == true)
+            {
+                gamemode = gmoption4.Text;
+            }
+
+            if (tmoption1.Checked == true)
+            {
+                time = tmoption1.Text;
+            }
+            else if (tmoption2.Checked == true)
+            {
+                time = tmoption2.Text;
+            }
+            else if (tmoption3.Checked == true)
+            {
+                time = tmoption3.Text;
+            }
+            else if (tmoption4.Checked == true)
+            {
+                time = tmoption4.Text;
+            }
+            else if (tmoption5.Checked == true)
+            {
+                time = tmoption5.Text;
+            }
+
+            if (nameBox.Text.Equals(""))
+            {
+                MessageBox.Show("Must enter name.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (genderBox.SelectedItem.ToString() == "<Select Gender>")
+            {
+                MessageBox.Show("Must select gender.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (unit.Count == 0)
+            {
+                MessageBox.Show("No unit selected.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (gamemode == "")
+            {
+                MessageBox.Show("Must select a gamemode.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (time == "")
+            {
+                MessageBox.Show("Must select a schedule.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (unit.Count > 0)
+            {
+                string unitMessage = "\nUnit: " + string.Join(", ", unit);
+                MessageBox.Show(
+                    "Name: " + nameBox.Text +
+                    "\nGender: " + genderBox.Text +
+                    "\nBirthdate: " + birthdateBox.Text + unitMessage +
+                    "\nGamemode: " + gamemode +
+                    "\nDate and Time: " + time,
+                    "War Thunder Even Scheduler",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void done_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
